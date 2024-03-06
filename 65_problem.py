e34688b32dcae37708c036cafd0b413bb223bc14
AKSHAT SRIVASTAVA <90958990+Akshat-ak47@users.noreply.github.com>
2024-03-06 10:11:45 +0530

    Uploaded the Day 65 Problem
    Solved the Day 65 Problem - Marbles in Jar
    Programming : Puzzles

---
+++ 65_problem.py
@@ -0,0 +1,14 @@
+'''
+Marbles in Jar
+
+You have two jars, 50 red marbles and 50 blue marbles. You need to place all the marbles into the jars such that when you blindly pick one marble out of one jar, you maximize the chances that it will be red. When picking, you’ll first randomly pick a jar, and then randomly pick a marble out of that jar. You can arrange the marbles however you like, but each marble must be in a jar. What is the maximum probability?
+
+Answer in decimal rounded off to 2 decimal places. For example, if the answer is 2/3, the response should be 0.67
+
+'''
+
+print('''To maximize the chances of picking a red marble, 
+      you should place one red marble in one jar and the rest of the marbles (49 red and 50 blue) 
+      in the other jar.)''')
+
+print('''So, the maximum probability of picking a red marble is : 0.75)''')
